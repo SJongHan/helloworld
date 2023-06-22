ebd979f2d85989843cfccd6bfad901222770dd45
SJongHan <116073055+SJongHan@users.noreply.github.com>
2023-06-22 16:33:54 +0900

    Update HelloWorld.java

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -3,7 +3,7 @@ package hello;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Worlddddddd!!!");
+		System.out.println("Hello Worldkkkkkkkkk");
 
 	}
 
